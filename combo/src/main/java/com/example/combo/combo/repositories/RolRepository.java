c7c4f6fc4a4debc886ca564951ebd89f93d013ff
Sebs_music_live <lopezortegajohansebastian@gmail.com>
2024-11-08 11:58:18 -0500

    Se solucionan errores

--- combo/src/main/java/com/example/combo/combo/repositories/RolRepository.java
+++ combo/src/main/java/com/example/combo/combo/repositories/RolRepository.java
@@ -3,6 +3,6 @@ package com.example.combo.combo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.combo.combo.entities.Rol;
-public interface RolRepository extends JpaRepository<Rol, Long>{
+public interface RolRepository extends JpaRepository<Rol, Integer>{
 
 }
